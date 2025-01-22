a8037f0d792ae66455acdba828a5d93bd88f170b
pkrolikowski <me@dupa.com>
2025-01-22 19:41:13 +0100

    another test

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+charts=$(find deploy-multi -type f -path "*/apps/*/Chart.yaml")
+results=()
+
+for chart in $charts; do
+	env=$(basename $(dirname $chart))
+	name=$(yq e '.name' "$chart")
+	ext_name="$env-$name"
+
+	#echo "${results[@]} => $ext_name"
+
+	if [[ " ${results[@]} " =~ " $ext_name " ]]; then
+		echo "$ext_name duplicated"
+		exit 1
+	else
+		results+=("$ext_name")
+	fi
+done
