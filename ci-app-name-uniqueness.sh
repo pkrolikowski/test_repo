43ed341b71c8533d50494ec90544d22bbd7dfa6b
pkrolikowski <me@dupa.com>
2025-01-23 19:39:26 +0100

    add tool

---
+++ ci-app-name-uniqueness.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+            charts=$(find deploy-multi -type f -path "*/apps/*/Chart.yaml")
+            results=()
+
+            for chart in $charts; do
+              env=$(basename $(dirname $chart))
+              name=$(yq e '.name' "$chart")
+              ext_name="$env-$name"
+
+              if [[ " ${results[@]} " =~ " $ext_name " ]]; then
+                echo "$ext_name duplicated"
+                exit 1
+              else
+                results+=("$ext_name")
+              fi
+            done
